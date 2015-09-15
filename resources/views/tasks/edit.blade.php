0004a683cb520c6d3c1d55eb7fb7df8cdab4bf66
GanjaGanja <ochenj.gorkyj@users.noreply.github.com>
2015-09-15 15:17:44 +0600

    add errors partial and make link to it in views: create.blade.php and edit.blade.php

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,6 +6,14 @@
 <p class="lead">Edit this task below. <a href="{{ route('tasks.index') }}">Go back to all tasks.</a></p>
 <hr>
 
+@include('partials.alerts.errors')
+
+@if(Session::has('flash_message'))
+    <div class="alert alert-success">
+        {{ Session::get('flash_message') }}
+    </div>
+@endif
+
 {!! Form::model($task, [
     'method' => 'PATCH',
     'route' => ['tasks.update', $task->id]
